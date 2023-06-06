55e379c04c06c9febd41b7f44732989014a0bd4a
fbarker92 <72350921+fbarker92@users.noreply.github.com>
2023-06-06 08:24:05 +0100

    Update EXOCreateDMARCSharedMailboxes.ps1

--- EXCHANGE-ONLINE/EXOCreateDMARCSharedMailboxes.ps1
+++ EXCHANGE-ONLINE/EXOCreateDMARCSharedMailboxes.ps1
@@ -1,14 +1,24 @@
+##Connect to Exchange and Msol
 Connect-ExchangeOnline
 Connect-MsolService
 
-$Domains - (Get-MsolDomain).Name
+##Get list of current domains in tenant
+$Domains = (Get-MsolDomain).Name
 
+##Create DMARC Forensics and Aggregate shared mailboxes 
 ForEach ($Domain in $Domains) {
     $Name = $Domain 
     $PrimaryEmailAddressForensics = 'DMARCForensics@' + $Domain
     $PrimaryEmailAddressAggregate = 'DMARCAggregate@' + $Domain
     
     New-Mailbox -Shared -Name "DMARC Forensics | $Name" -PrimarySmtpAddress $PrimaryEmailAddressForensics
-    New-Mailbox -Shared -Name "DMARC Aggregate | $Name" -PrimarySmtpAddress $PrimaryEmailAddressAggregate
-    
+    New-Mailbox -Shared -Name "DMARC Aggregate | $Name" -PrimarySmtpAddress $PrimaryEmailAddressAggregate    
     }
+
+
+##Hide DMARC Mailboxes from GAL
+$DMARCMbxs = Get-Mailbox -Filter {PrimarySmtpAddress -like "DMARC*"}
+
+ForEach (DMARCMbx in $DMARCMbxs) {
+   Set-Mailbox -Identity $DMARCMbx -HiddenFromAddressListsEnabled $true
+   }
